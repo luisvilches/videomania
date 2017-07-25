c31394a8ac04603fec36d1a291107ebdcf15893a
LuisVilches <lvilches21@gmail.com>
2017-07-24 20:21:17 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     port: process.env.PORT || 4000,
     db: {
-        connection: "mongodb://admin:admin@ds013941.mlab.com:13941/desarrollo"
+        connection: "mongodb://videomanias:videomanias@ds111123.mlab.com:11123/videomanias"
     },
     TOKEN_SECRET: process.env.TOKEN_SECRET || "79d27Mh1swkXnqGp2GdB0Xl4zmTKox2T"
 }
